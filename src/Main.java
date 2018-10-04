a4feb2b2b43eb3bf28be49d36f24c41c832b9a16
romielisse <romielisse@gmail.com>
2018-10-04 10:33:41 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    // Declare new Scanner object to read in input
+    Scanner in = new Scanner(System.in);
+
+    // Declare array list to hold list of movies
+    ArrayList<String> Movies = new ArrayList<>();
+
+    // Declare variable "movie_title" to hold movie entered by the user
+    String movieTitle = "";
+
+    do {
+      System.out.print("Enter movie or Q(quit): ");
+      movieTitle = in.nextLine();
+
+      // Add movie to array list if they did not enter "Q"
+      if(!movieTitle.equalsIgnoreCase("Q")) {
+        Movies.add(movieTitle);
+      }
+
+    }while(!movieTitle.equalsIgnoreCase("Q"));
+
+    // Sort list of movies
+    Collections.sort(Movies);
+
+    // Print list of movies
+    for(String s:Movies){
+      System.out.println(s);
+    }
+
+    // Randomly select movie from list for user to watch
+    Random rand = new Random();
+    // Generate random integer for index
+    int index = rand.nextInt(Movies.size()-1);
+    String movieToWatch = Movies.get(index);
+
+    System.out.println("\nYou should watch " + movieToWatch + " today!");
+  }
+}
